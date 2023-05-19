c268842f9200ce4a8b5bb235c75107e5f41de274
souzap371 <souzap371@gmail.com>
2023-05-18 21:59:09 -0400

    Desenvolvimento de home page e authForm

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: const HomePage(title: 'My Chat GPT'),
+      home: const HomePage(),
       debugShowCheckedModeBanner: false,
     );
   }
--- lib/pages/homePage.dart
+++ lib/pages/homePage.dart
@@ -1,110 +1,49 @@
+import 'package:app_gpt/pages/authForm.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatefulWidget {
-  const HomePage({super.key, required this.title});
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
 
-  final String title;
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    final GlobalKey<FormState> formKey = GlobalKey<FormState>();
-
     return Scaffold(
-      backgroundColor: Colors.green[700],
-      body: Padding(
-        padding: const EdgeInsets.only(top: 200),
-
-        child: Form(
-          key: formKey,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Digite seu E-mail',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Valor inválido.';
-                    }
-                    return null;
-                  },
-                ),
-                const SizedBox(
-                  height: 30,
-                ),
-                TextFormField(
-                  decoration: const InputDecoration(
-                    hintText: 'Digite sua senha',
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide(color: Colors.white),
-                    ),
-                  ),
-                  validator: (String? value) {
-                    if (value == null || value.isEmpty) {
-                      return 'Valor inválido.';
-                    }
-                    return null;
-                  },
-                ),
-                const Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  child: ElevatedButton(
-                    onPressed: null,
-                    child: Text('Login'),
-                  ),
-                ),
-              ],
+      body: Stack(
+        children: [
+          Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                colors: [
+                  Color(0xffac13c5),
+                  Color(0xff4b0082),
+                ],
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+              ),
             ),
           ),
-        ),
-        // child: Center(
-        //   child: Column(
-        //     //mainAxisAlignment: MainAxisAlignment.center,
-        //     children: const <Widget>[
-        //       Text(
-        //         'Bem vindo ao seu Chat GPT',
-        //       ),
-        //       SizedBox(
-        //         height: 50,
-        //       ),
-        //       TextField(
-        //         autofocus: true,
-        //         decoration: InputDecoration(
-        //           border: OutlineInputBorder(
-        //             borderSide: BorderSide(color: Colors.white),
-        //           ),
-        //           icon: Icon(Icons.person),
-        //           hintText: ('Digite o e-mail'),
-        //         ),
-        //       ),
-        //       SizedBox(
-        //         height: 10,
-        //       ),
-        //       TextField(
-        //         autofocus: true,
-        //         obscureText: true,
-        //         decoration: InputDecoration(
-        //           border: OutlineInputBorder(
-        //             borderSide: BorderSide(color: Colors.white),
-        //           ),
-        //           icon: Icon(Icons.lock_outline),
-        //           hintText: ('Digite a senha'),
-        //         ),
-        //       ),
-        //     ],
-        //   ),
-        // ),
+          Center(
+            child: SingleChildScrollView(
+              child: SizedBox(
+                width: double.infinity,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      // child: Image.asset(
+
+                      // ),
+                    ),
+                    const AuthForm(),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
